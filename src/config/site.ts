49c1299e68176f56c21077d1493547dbd21b2744
Johnny Camatón Rendón <jhonnyalbert245@gmail.com>
2026-03-01 15:51:25 -0500

    updated page

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,10 @@ export const siteConfig = {
     AVAILABLE_FOR_WORK: true,
     name: "Johnny Camatón",
     handle: "sammy2455",
-    title: {es: "Ingeniero en TI · Máster en Ciberseguridad", en: "IT Engineer · Master's in Cybersecurity"},
+    title: {
+        es: "Ingeniero en TI · Máster en Ciberseguridad",
+        en: "IT Engineer · Master's in Cybersecurity"
+    },
     location: "La Libertad, Santa Elena, Ecuador",
     email: "jhonnyalbert245@gmail.com",
     github: "https://github.com/sammy2455",
